20909ab27f89309453238a3cf77f4b03ba4b42ab
YashPradhan77 <83752766+YashPradhan77@users.noreply.github.com>
2024-12-11 17:35:43 +0530

    configured cicd

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -67,7 +67,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.public_subnet[*].id
+    subnet_ids = module.vpc.private_subnets
   }
 }
 
@@ -75,7 +75,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name  = aws_eks_cluster.eks_cluster.name
   node_role_arn = aws_iam_role.eks_node_group_role.arn
-  subnet_ids    = aws_subnet.public_subnet[*].id
+  subnet_ids    = module.vpc.private_subnets
   scaling_config {
     desired_size = 2
     max_size     = 3
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,38 +1,20 @@
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/24"
-}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.eks_vpc.id
-}
+  name = "ecs-vpc"
+  cidr = "10.0.0.0/16"
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.eks_vpc.id
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
+  
 
-resource "aws_subnet" "public_subnet" {
-  count                   = 2
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.${count.index}.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
-}
-
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet[0].id
-}
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.eks_vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
