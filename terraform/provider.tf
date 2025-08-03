b0bbbf61674bb862ec03689af0bf11924cde52ae
Yash Pradhan <yashpradhan@Yashs-MacBook-Air.local>
2025-08-03 15:48:15 +0530

    organising

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.67.0"
-    }
-  }
-  backend "s3" {
-    bucket = "eks-voting-app-terraform-bucket"
-    key = "key/terraform.tfstate"
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "5.67.0"
+#     }
+#   }
+#   backend "s3" {
+#     bucket = "eks-voting-app-terraform-bucket"
+#     key = "key/terraform.tfstate"
+#   }
+# }
 
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
